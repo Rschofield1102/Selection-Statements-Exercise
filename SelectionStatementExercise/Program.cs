100ea63460849496b7756f10a10d0b9ca3ca79db
Robert Schofield <rschofield1992@gmail.com>
2024-03-27 14:26:20 -0400

    finished

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -3,12 +3,10 @@
     public class Program
     {
         static void Main(string[] args)
-        {
+        {  //exercise 1
 
-
-
-            Console.WriteLine("Welcome to guess the number!");
-            Console.WriteLine("Guess the correct number!");
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Guess the correct number between 1 and 10!");
 
             var userResponse = Console.ReadLine();
 
@@ -16,26 +14,69 @@
 
             var random = new Random();
             var number = 7;
-            
 
-            
-            
+            if (guess > number)
+            {
+                Console.WriteLine($"Im sorry, {userResponse} is too HIGH");
 
-                if (guess > number)
-                {
-                    Console.WriteLine("too HIGH");
-                
+            }
+            if (guess < number)
+            {
+                Console.WriteLine($"Im sorry, {userResponse} is too Low");
+            }
+            if (guess == number)
+            {
+                Console.WriteLine("good job");
+            }
+            Console.WriteLine();
+
+            //exercise 2
+            Console.WriteLine(" What is your favorite subject in school?");
+            var subject = Console.ReadLine();
+            switch (subject)
+               {
+                case "Math":
+                    Console.WriteLine("Math is easy!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is one of my favorites too!");
+                    break;
+                case "P.E":
+                    Console.WriteLine("P.E. is fun");
+                    break;
+                case "Language Arts":
+                    Console.WriteLine("Ohhh");
+                    break;
+                case "Homeroom":
+                    Console.WriteLine("HAAAA");
+                    break;
+                default:
+                    Console.WriteLine($"Ahh {subject} is fun too!");
+                    break;
                 }
-                else if (guess < number)
-                {
-                    Console.WriteLine("too Low");
-                }
-                else
-                {
-                    Console.WriteLine("good job");
-                }
-
+            
+               
 
             
+
+
+        
+
+
+
+
         }
-    }    }
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
