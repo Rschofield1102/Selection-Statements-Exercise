8302cb94e53b5794db5bda913619f24d267e822e
Robert Schofield <rschofield1992@gmail.com>
2024-03-27 13:21:10 -0400

    commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+            Console.WriteLine("Welcome to guess the number!");
+            Console.WriteLine("Guess the correct number!");
+
+            var userResponse = Console.ReadLine();
+
+            var guess = int.Parse(userResponse);
+
+            var random = new Random();
+            var number = 7;
+            
+
+            
+            
+
+                if (guess > number)
+                {
+                    Console.WriteLine("too HIGH");
+                
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("too Low");
+                }
+                else
+                {
+                    Console.WriteLine("good job");
+                }
+
+
+            
         }
-    }
-}
+    }    }
